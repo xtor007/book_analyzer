9d5dd24ce38df92ab5db0f9e5874150da8524fae
berlinsky <berlinsky.yar@gmail.com>
2022-06-15 15:53:47 +0300

    creating table for parameters
    some parameters may be changed in some time

--- program/DBWorker.py
+++ program/DBWorker.py
@@ -1,4 +1,6 @@
 import pymysql
+
+
 class DBWorker:
     __host = ""
     __user = ""
@@ -6,7 +8,7 @@ class DBWorker:
     __password = ""
     __db_name = ""
 
-    def __init__ (self, host ="localhost", user ="root", port ="3306", password ="", db_name ="BooksMeasurement"):
+    def __init__(self, host="localhost", user="root", port="3306", password="", db_name="BooksMeasurement"):
         self.__host = host
         self.__user = user
         self.__port = int(port)
@@ -24,6 +26,24 @@ class DBWorker:
                 cursorclass=pymysql.cursors.DictCursor
             )
             print("Successfully connected!")
+
+            try:
+                with connection.cursor() as cursor:
+                    drop_script = "DROP TABLE IF EXISTS Parameters;"
+                    create_script = "CREATE TABLE Parameters (   `id` INT NOT NULL,   " \
+                                    "`name` VARCHAR(150) NOT NULL,   `year` INT NOT NULL,   `pages` INT NOT NULL,   " \
+                                    "`rating` INT NOT NULL,   `isRealistic` TINYINT NOT NULL,   `charactersNumber` " \
+                                    "INT NOT NULL,   `femaleNumber` INT NOT NULL,   `maleNumber` INT NOT NULL,   " \
+                                    "`plotTwists` INT NOT NULL,   `raceNumber` INT NOT NULL,   `friendsNumber` INT " \
+                                    "NOT NULL,   `lovesNumber` INT NOT NULL,   `relativesNumber` INT NOT NULL,   " \
+                                    "`enemiesNumber` INT NOT NULL,   `locationsNumber` INT NOT NULL,   PRIMARY KEY (" \
+                                    "`id`)) "
+                    cursor.execute(drop_script)
+                    cursor.execute(create_script)
+
+            finally:
+                connection.close()
+
         except Exception as ex:
             print("Connection refused...")
             print(ex)
