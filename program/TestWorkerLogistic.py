b467e62cc1ea565c564e94574d109def040b8acd
Anatoliy <tolxpams@gmail.com>
2022-06-20 00:58:36 +0300

    final

--- program/TestWorkerLogistic.py
+++ program/TestWorkerLogistic.py
@@ -1,11 +1,15 @@
 from FileWorker import MaxMinFile, ParamFile, CellFile
 import math
+import matplotlib.pyplot as plt
 
 class TestWorkerLogistic:
 
     data = []
     names = []
     results = []
+
+    x = []
+    y = []
 
     paramNames = [
         "year", "pages",
@@ -56,12 +60,21 @@ class TestWorkerLogistic:
 
     def goTesting(self):
         self.testLogisticReg()
+        plt.scatter(self.x, self.y)
+        #plt.scatter(self.x, self.results)
+        plt.show()
         self.testTree()
 
     def testTree(self):
         resultNames = ["Light", "Dark"]
         print("----------------------------------")
         print("Start testing tree")
+        rss = 0
+        tss = 0
+        avarageY = 0
+        for i in range(len(self.data)):
+            avarageY += self.results[i]
+        avarageY /= len(self.data)
         rightAnswers = 0
         path = "solution/tree/"
         for i in range(len(self.data)):
@@ -82,11 +95,26 @@ class TestWorkerLogistic:
                 else:
                     fileName += "l"
             print(f'{self.names[i]} is {resultNames[self.results[i]]}; Prediction is {resultNames[predict]}')
+            rss += (self.results[i] - avarageY) ** 2
+            tss += (self.results[i] - predict) ** 2
             if self.results[i] == predict:
                 rightAnswers += 1
         print(f'Correctness of predictions - {rightAnswers / len(self.data)}')
+        print(f'RSS = {rss}, TSS = {tss}, R2 = {1 - rss / tss}')
 
     def testLogisticReg(self):
+        rozp = [0]*21
+        xInGisto = []
+        xValInGisto = -1
+        for _ in range(21):
+            xInGisto.append(xValInGisto)
+            xValInGisto += 0.1
+        rss = 0
+        tss = 0
+        avarageY = 0
+        for i in range(len(self.data)):
+            avarageY += self.results[i]
+        avarageY /= len(self.data)
         resultNames = ["Light", "Dark"]
         print("----------------------------------")
         print("Start testing logistic regresion")
@@ -94,11 +122,21 @@ class TestWorkerLogistic:
         parametrs = paramFile.getParams()
         rightAnswers = 0
         for i in range(len(self.data)):
-            prediction = self.expon(parametrs,self.data[i])
+            predictionVal = self.expon(parametrs,self.data[i])
+            rozp[int((predictionVal-self.results[i])*10) + 10] += 1
+            if predictionVal>0.5:
+                prediction = 1
+            else:
+                prediction = 0
             print(f'{self.names[i]} is {resultNames[self.results[i]]}; Prediction is {resultNames[prediction]}')
+            rss += (self.results[i] - avarageY) ** 2
+            tss += (self.results[i] - predictionVal) ** 2
             if self.results[i] == prediction:
                 rightAnswers += 1
+        plt.bar(xInGisto,rozp,0.4)
+        plt.show()
         print(f'Correctness of predictions - {rightAnswers/len(self.data)}')
+        print (f'RSS = {rss}, TSS = {tss}, R2 = {1 - rss/tss}')
 
     def expon(self, parametrs, example):
         z = 0
@@ -107,7 +145,6 @@ class TestWorkerLogistic:
                 z += parametrs[i]
             else:
                 z += parametrs[i] * example[i]
-        if (1 / (1 + (math.e ** (-z)))) > 0.5 :
-            return 1
-        else:
-            return 0
+        self.x.append(z)
+        self.y.append((1 / (1 + (math.e ** (-z)))))
+        return (1 / (1 + (math.e ** (-z))))
